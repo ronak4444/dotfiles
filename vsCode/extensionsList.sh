31291e35bbe37f0d03cf6ab719b5a127c76f3784
mac OS <macos@macOS.local>
2020-02-01 10:31:58 +0530

    vscode extensonlist.sh for install added

---
+++ vsCode/extensionsList.sh
@@ -0,0 +1,11 @@
+code --install-extension ms-python.python
+code --install-extension ms-vscode.go
+code --install-extension amodio.gitlens
+code --install-extension vscodevim.vim
+code --install-extension njpwerner.autodocstring
+code --install-extension formulahendry.code-runner
+code --install-extension ms-python.anaconda-extension-pack
+code --install-extension aaron-bond.better-comments
+code --install-extension visualstudioexptteam.vscodeintellicode
+code --install-extension ms-vsliveshare.vsliveshare
+code --install-extension ms-vsliveshare.vsliveshare-pack
